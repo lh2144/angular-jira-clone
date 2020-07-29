a14ea5778af4a045b2f47d823faa48768e5380ab
lh2144 <hlian@stevens.edu>
2020-07-28 22:36:14 -0500

    changes

---
+++ back-end/typescript-starter/dist/project/project.controller.d.ts
@@ -0,0 +1,30 @@
+export declare class ProjectController {
+    getProject(): {
+        id: string;
+        name: string;
+        url: string;
+        description: string;
+        category: string;
+        createdAt: string;
+        updatedAt: string;
+        users: {
+            id: string;
+            name: string;
+            avatarUrl: string;
+            projectId: string;
+        }[];
+        issues: {
+            id: string;
+            title: string;
+            description: string;
+            type: string;
+            status: string;
+            priority: string;
+            listPosition: number;
+            createdAt: string;
+            updatedAt: string;
+            reporterId: string;
+            userIds: string[];
+        }[];
+    };
+}
