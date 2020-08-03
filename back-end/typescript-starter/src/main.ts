d10608f193e79778ff47d6a85c3ddda887f72a30
lh2144 <hlian@stevens.edu>
2020-08-03 01:02:20 -0500

    changes

--- back-end/typescript-starter/src/main.ts
+++ back-end/typescript-starter/src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
-const corsOptions: CorsOptions = {
-    origin: ['http://localhost:4200', /.trungk18.com/],
-};
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {
-        cors: corsOptions
-    });
+    const app = await NestFactory.create(AppModule, { cors: true });
     await app.listen(3000);
 }
 bootstrap();
